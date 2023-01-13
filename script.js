d8f103e65c0fd4545fca2f72a1d3ab575ccf5844
ZubeidaDhupli <zubeidadhupli@outlook.com>
2023-01-13 17:34:42 +0000

    add password option prompts

--- script.js
+++ script.js
@@ -88,14 +88,67 @@ var upperCasedCharacters = [
   'Z'
 ];
 
+const passwordLength = prompt("Between 10 characters and 64 characters, what length would you like your password to be?");
+const specialCharsPrompt = prompt("Would you like to use Special Characters in your password?");
+const numericCharsPrompt = prompt("Would you like to use Numeric Characters in your password?");
+const lowerCasedCharsPrompt = prompt("Would you like to use Lower Cased Characters in your password?");
+const upperCasedCharsPrompt = prompt("Would you like to use  Upper Cased Characters in your password?"); 
+
 // Function to prompt user for password options
 function getPasswordOptions() {
+  passwordLength;
+  specialCharsPrompt;
+  numericCharsPrompt;
+  lowerCasedCharsPrompt;
+  upperCasedCharsPrompt;
 
+  if (passwordLength.isNAN === false) {
+    alert("Please enter a number between 10 and 64.");
+  } else if ((passwordLength >= 10) || (passwordLength <= 64)) {
+    alert("You have entered a number that is not between 10 and 64.")
+  } else {
+    alert("Thank you for choosing a length for your password.");
+  }
+  if (specialCharsPrompt === false) {
+    alert("You have chosen to not include a Special Character in your password.");
+  } else if(specialCharsPrompt !== specialCharacters) {
+    alert("Please enter Special Characters from this selection: " + specialCharacters + ".");
+  } else {
+    alert("Thank you for choosing your Special Character(s)");
+  }
+  if (numericCharsPrompt === false) {
+    alert("You have chosen to not include a Numeric Character in your password.");
+  } else if(numericCharsPrompt !== numericCharacters) {
+    alert("Please enter Numeric Characters from this selection: " + numericCharacters + ".");
+  } else {
+    alert("Thank you for choosing your Numeric Character(s)");
+  }
+  if (lowerCasedCharsPrompt === false) {
+    alert("You have chosen to not include a Lower Cased Character in your password.");
+  } else if(lowerCasedCharsPrompt !== lowerCasedCharacters) {
+    alert("Please enter Lower Cased Characters from this selection: " + lowerCasedCharacters + ".");
+  } else {
+    alert("Thank you for choosing your Lower Cased Character(s)");
+  }  
+  if (upperCasedCharsPrompt === false) {
+    alert("You have chosen to not include an Upper Cased Character in your password.");
+  } else if(upperCasedCharsPrompt !== upperCasedCharacters) {
+    alert("Please enter Upper Cased Characters from this selection: " + upperCasedCharacters + ".");
+  } else {
+    alert("Thank you for choosing your Upper Cased Character(s)");
+  }
+  return;
 }
+
+getPasswordOptions();
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-
+  // randomIndex gets a random index value
+  var randomIndex = Math.floor(Math.random() * arr.length);
+  // item gets a random from the array
+  var item = arr[randomIndex];
+  return item;
 }
 
 // Function to generate password with user input
